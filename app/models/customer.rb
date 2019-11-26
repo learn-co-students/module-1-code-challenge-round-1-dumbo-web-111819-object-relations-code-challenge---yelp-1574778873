243c20b83f01c832c33d7f4f91a43d804390dae3
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-26 10:47:31 -0500

    finished :(

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,103 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def restaurants
+
+  end
+
+  def add_review(resturant, rating)
+    # create a new review (Review.new).
+
+    Review.new(self, resturant, rating)
+  end
+
+  def num_reviews
+#   - Returns the total number of reviews that a customer has authored
+# iterate through Review.all to FIND the total num of reviews a customer wrote.
+  total = 0
+  Review.all.find do |num|
+    total += num.rating
+    end
+    total
+  end
+
+  def self.find_all_by_name(name)
+    # use find loop to find the first customer whose full name matches.
+     Customer.all.find do |name|
+       self.full_name == name
+    # binding.pry
+    end
+  end
+
+  def self.find_all_by_given_name(name)
+    # use map to return an array that has all of the customers with
+    # the same name.
+    Customer.all.map do |name|
+      self.given_name == name
+    end
+  end
+
+  def resturants
+
+  end 
 end
+
+
+# - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# - `Customer#initialize`
+#   - Customer should be initialized with a given name and a family name, both strings
+# (i.e., first and last name, like George Washington)"
+# - `Customer#given_name`
+#   - returns the customer's given name
+#   - should be able to change after the customer is created
+# - `Customer#family_name`
+#   - returns the customer's family name
+#   - should be able to change after the customer is created
+# - `Customer.all`
+#   - returns **all** of the customer instances
+# - `Customer#full_name`
+#   - returns the full name of the customer, with the given name and the family name concatenated, Western style.
+# - `Customer#add_review(restaurant, rating)`
+#   - given a **restaurant object** and a star rating (as an integer),
+#   creates a new review and associates it with that customer and restaurant.
+# - `Customer.find_by_name(name)`
+#   - given a string of a **full name**, returns the **first customer** whose full name matches
+# - `Customer.find_all_by_given_name(name)`
+#   - given a string of a given name, returns an **array** containing all customers with that given name
+# - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,54 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    # used map to return a new array of all the reviews that matched the specific
+    # restaurant (self).
+    Review.all.map do |review|
+      review.rating == self
+    end
+  end
+
+  def customers
+    # returns a unique array of all customers (Review.all)
+    # who have reviewed the specific resturant instance.
+    uniq_customer_array = []
+    Review.all.map do |customer|
+  uniq_customer_array << customer.rating == self
+    end
+    uniq_customer_array.uniq
+  end
+  # binding.pry
 end
+
+
+
+# - `Restaurant#customers`
+#   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+# - `Restaurant#average_star_rating`
+#   - returns the average star rating for a restaurant based on its reviews
+#   - Reminder: you can calculate the average by adding up all the ratings and dividing by the number of ratings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# - `Restaurant#initialize`
+#   - Restaurants should be initialized with a name, as a string
+# - `Restaurant#name`
+#   - returns the restaurant's name
+#   - should not be able to change after the restaurant is created
+# - `Restaurant#reviews`
+#   - returns an array of all reviews for that restaurant
