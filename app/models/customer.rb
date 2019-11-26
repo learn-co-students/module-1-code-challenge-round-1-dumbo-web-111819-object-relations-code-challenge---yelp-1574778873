761f0ed6c53675fb774d08daefe4db04e708dc20
ajshemi <akpojotor@yahoo.com>
2019-11-26 10:48:10 -0500

    challange commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all=[]
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    Review.all.select {|review_instance| review_instance.customer==self}.uniq
+  end
+
+  def add_review(restaurant,rating)
+    Review.new(self,restaurant,rating)
+  end
+
+  def num_reviews
+    Review.all.select {|review_instance| review_instance.customer==self}.count
+  end
+  def self.find_by_name(full_name)
+    first_name=full_name.split[0]
+    given_name=full_name.split[1]
+    customer_reviews=self.all.select{|review_instance| review_instance.customer==first_name}
+
+  end
+
+  def self.find_all_by_given_name(name)
+    first_name=full_name.split[0]
+    given_name=full_name.split[1]
+    customer_reviews=self.all.select {|review_instance| review_instance.customer==given_name}
+  
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,38 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+c1=Customer.new("John","Doe")
+c2=Customer.new("Peter","Pan")
+c3=Customer.new("Jack","Smith")
+c4=Customer.new("Mark","Lee")
+c5=Customer.new("Mark","Lee")
+
+Res1=Restaurant.new("Thai Chick")
+Res2=Restaurant.new("Pasta Cheese")
+Res3=Restaurant.new("Soup Kitchen")
+Res4=Restaurant.new("Soup Kitchen")
+
+rev1=Review.new(c1,Res1,8)
+rev2=Review.new(c2,Res2,4)
+rev3=Review.new(c3,Res3,5)
+rev4=Review.new(c4,Res4,6)
+rev5=Review.new(c5,Res1,5)
+rev6=Review.new(c1,Res2,7)
+rev7=Review.new(c2,Res3,5)
+rev8=Review.new(c3,Res4,8)
+rev9=Review.new(c4,Res1,5)
+rev10=Review.new(c5,Res2,9)
+
+c5.add_review(rev6,6)
+
+
+
+
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
