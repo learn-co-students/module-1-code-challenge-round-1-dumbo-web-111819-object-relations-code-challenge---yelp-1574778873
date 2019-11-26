0d61daab586d5ac27b4a9e48fd977766258e2a89
Annie <anniezheng.anniee@gmail.com>
2019-11-26 10:46:49 -0500

    deliverables done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,43 @@ class Restaurant
     @name = name
   end
 
+  def customers 
+    cus_arr= self.reviews.map do |review|
+      review.customer
+    end
+    cus_arr.uniq
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    total_review = self.reviews.length
+    rating_arr = self.reviews.map do |review|
+      review.rating
+    end
+    rating_arr.sum/total_review
+  end
+
 end
+
+### D O N E ###
+# #### Restaurant
+
+# - `Restaurant#initialize`
+#   - Restaurants should be initialized with a name, as a string
+# - `Restaurant#name`
+#   - returns the restaurant's name
+#   - should not be able to change after the restaurant is created
+# #### Restaurant
+
+# - `Restaurant#reviews`
+#   - returns an array of all reviews for that restaurant
+# # - `Restaurant#customers`
+# #   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+# - `Restaurant#average_star_rating`
+#   - returns the average star rating for a restaurant based on its reviews
+#   - Reminder: you can calculate the average by adding up all the ratings and dividing by the number of ratings
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,49 @@
+#### Review
+
+# - `Review#customer`
+#   - returns the customer object for that review
+#   - Once a review is created, should not be able to change the customer
+# - `Review#restaurant`
+#   - returns the restaurant object for that given review
+#   - Once a review is created, should not be able to change the restaurant
+
 class Review
-  
+    attr_reader :customer, :restaurant, :rating
+
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating= rating
+
+        @@all << self
+    end
+
+    def customer 
+        Review.all.select do |review|
+            review.customer
+        end
+    end
+
+    def restaurant
+        Review.all.select do |review|
+            review.restaurant
+        end
+    end
+
+    def self.all 
+        @@all
+    end
+
 end
+
+### D O N E ###
+# #### Review
+
+# - `Review#initialize`
+#   - Reviews should be initialized with a customer, restaurant, and a rating (a number)
+# - `Review#rating`
+#   - returns the rating for a restaurant.
+# - `Review.all`
+#   - returns all of the reviews
