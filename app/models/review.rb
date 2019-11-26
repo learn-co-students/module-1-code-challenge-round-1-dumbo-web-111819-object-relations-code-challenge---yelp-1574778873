78a65b78419cf8e2f8ee10a1911fc22f5466bf8a
mk17173n <Monsur.Khan@gmail.com>
2019-11-26 10:49:28 -0500

    deliverables done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,27 @@
 class Review
-  
+
+    attr_accessor :customer : restaurant :rating :rating
+
+    @@all = self
+
+    def initialize (customer, restaurant, rating)
+        @customer  = customer
+        @restaurnat  = restaurant
+        @rating = rating
+     @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def review_customer
+        customer.review = self
+    end
+
+    def review_restaurant
+        restaurant.review = self
+    end
+
 end
