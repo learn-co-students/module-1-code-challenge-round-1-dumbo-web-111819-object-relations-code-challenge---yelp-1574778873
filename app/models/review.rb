4a229d4af81b528f5edac721c310026cf4aef940
keixcruick90 <cruickshankxkeillan90@gmail.com>
2019-11-26 10:47:02 -0500

    Done?

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,35 @@
 class Review
-  
+
+    attr_accessor :customer, :restaurant
+    attr_writer :rating
+
+    @@all = []
+
+  def initialize(customer, restaurant, rating)
+      @customer = customer
+      @restaurant = restaurant
+      @rating = rating
+      @@all << self
+  end
+
+  def customer
+      @customer
+  end
+
+  def restaurant
+      @restaurant
+  end
+
+  def rating
+      @rating
+  end
+
+  def customers
+     Review.all.map { |customer| review.customer}
+  end
+
+
+  def self.all
+      @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+jane = Customer.new("Jane", "Foster")
+ann = Customer.new("Ann", "Ryan")
+amy = Customer.new("Amy", "Adams")
+
+chilis = Restaurant.new("Chili's")
+patron = Restaurant.new("Patron")
+og = Restaurant.new("Olive Garden")
+bww = Restaurant.new("Buffalo Wild Wings")
+
+review1 = jane.add_review(jane, chilis, 5)
+review2 = jane.add_review(ann, og, 2)
+review3 = jane.add_review(amy, patron, 9)
+review4 = jane.add_review(amy, bww, 7)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
