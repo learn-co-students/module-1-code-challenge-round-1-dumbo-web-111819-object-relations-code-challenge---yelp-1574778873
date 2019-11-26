82f20d9fd5d0b9d50883227a520cde4a0a4a97f9
Baruch Phillips <baruchphillips@gmail.com>
2019-11-26 10:54:36 -0500

    some deliverables done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
   attr_reader :name
+  @@all = [] 
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+  def self.all 
+    @@all 
+  end 
 
+    def reviews
+      Review.all.find_all {|review| review.restaurant == self}
+  # find the restaurant in Review and return associated reviews
+    end 
+
+     def customers
+      customer_array = []
+      Review.all.map do |review|
+        if review.restaurant == self 
+        customer_array << review.customer
+        end 
+      end 
+     customer_array.uniq
+     end 
+    #   Logic: 
+    #  find all the customers for a restaurant 
+    #  Go through the reviews find the restaurant that matches the instance variable 
+    #  then push the customer who wrote it onto an array and return the array
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,22 @@
 class Review
+  attr_accessor  :rating
+  attr_reader :customer, :restaurant
+  @@all = []
+  def initialize(customer,restaraunt,rating)
+      @customer = customer 
+      @restaurant = restaraunt
+      @rating = rating 
+      @@all << self 
+  end
+  def self.all 
+    @@all 
+  end 
   
-end
+end 
+
+
+    #  Just find customer in self
+    # find the review that equals self
+    # and return customer
+    # Review.all.first.customer to reach customer
+    # use find
