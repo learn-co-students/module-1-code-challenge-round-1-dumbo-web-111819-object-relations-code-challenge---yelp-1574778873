bce1c7321d30d488af153d9a626d6963be3b48e3
github username <github email address>
2019-11-26 10:47:19 -0500

    yikes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,26 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def restaurants
+    Customer.all.uniq do |restaurant| 
+      restaurant.review == resraurant
+    end
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,18 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+c1 = Customer.new("Eric", "Cave")
+c2 = Customer.new("Jerry","Rice")
+c3 = Customer.new("Rod", "Woodson")
+
+re1 = Restaurant.new("Five Guys")
+re2 = Restaurant.new("Wendy's")
+re3 = Restaurant.new("Burger")
+
+rev1 = Review.new(c1, re1, 10)
+rev2 = Review.new(c2, re2, 8)
+rev3 = Review.new(c3, re3, 5)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
